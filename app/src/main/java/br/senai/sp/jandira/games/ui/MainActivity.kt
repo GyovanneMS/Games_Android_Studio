89c42aee6e36aad5cc11fce369a9389b196c3073
Gyovanne Martins Sabará <Gyovannecontato@gmail.com>
2022-11-27 09:51:43 -0300

    Update MainActivity.kt

--- app/src/main/java/br/senai/sp/jandira/games/ui/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/games/ui/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
         clienteRepository = ClienteRepository(this)
         val dados = getSharedPreferences("dados", MODE_PRIVATE)
